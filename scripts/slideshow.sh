d79a90d119ade893fbb62aa7c56a3d15cad72d79
Ian King <ian.antking@gmail.com>
2021-12-28 13:11:27 +0000

    add slideshow scripts

---
+++ scripts/slideshow.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+aws s3 sync s3://king-family-photos-dev /home/pi/Pictures
+feh \
+  --recursive \
+  --fullscreen \
+  --quiet \
+  --hide-pointer \
+  --slideshow-delay 60 \
+  /home/pi/Pictures
