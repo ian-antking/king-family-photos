642cf1ea23f3c74e5124bdbf4dd296a00ed8e58d
Ian King <ian.antking@gmail.com>
2022-01-05 10:30:10 +0000

    resize uploaded photos

--- resizePhoto/go.mod
+++ resizePhoto/go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/aws/aws-sdk-go v1.42.25
 )
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+)
--- resizePhoto/processor/resizer.go
+++ resizePhoto/processor/resizer.go
@@ -1,12 +1,42 @@
 package processor
 
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/png"
+
+	"github.com/nfnt/resize"
+)
+
 type Resizer struct {
 	width  uint
 	height uint
 }
 
-func (r *Resizer) Run(image Image) (Image, error) {
-	return image, nil
+func (r *Resizer) Run(imageInput Image) (Image, error) {
+	img, _, decodeErr := image.Decode(bytes.NewReader(imageInput.Image))
+	if nil != decodeErr {
+		return Image{}, fmt.Errorf("error decoding image: %s", decodeErr.Error())
+	}
+
+	resizedImage := resize.Resize(r.width, r.height, img, resize.Lanczos3)
+
+	buffer := new(bytes.Buffer)
+	encodeErr := jpeg.Encode(buffer, resizedImage, nil)
+
+	if nil != encodeErr {
+		return Image{}, fmt.Errorf("error encoding image: %s", decodeErr.Error())
+	}
+
+	newImage := Image{
+		Image:  buffer.Bytes(),
+		Bucket: imageInput.Bucket,
+		Key:    imageInput.Key,
+	}
+
+	return newImage, nil
 }
 
 func NewResizer(width, height uint) Resizer {
